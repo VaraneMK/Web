b21b980bd9adb32cb1b9ebd0027b3754e24f9854
Artem <grodex234@gmail.com>
2021-11-09 19:22:07 +0300

    lessons

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-"use strict";
+"use strict"
+const numberofFilms = +prompt("Сколько фильмов вы посмотрели?", "");
+const personalMovieDB = {
+    count: numberofFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const a = prompt("Один из последних фильмов?", ""),
+    b = prompt("На сколько оцените его?", ""),
+    c = prompt("Один из последних фильмов?", ""),
+    d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[с] = d;
+
+console.log(personalMovieDB);
